fdabbdff0569e842e26bc4c94c113a3bea0a53d3
arabambi <arababs2015@gmail.com>
2022-03-23 12:58:36 +0100

    login validation

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,44 +1,60 @@
-import React, { useContext, useEffect } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import { Link, useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import React, { useContext, useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-import Button from '../../components/buttons/Button';
-import CheckboxInput from '../../components/inputs/basic/Checkbox';
-import Input from '../../components/inputs/basic/Input';
-import PasswordInput from '../../components/inputs/basic/Password';
-import { UserContext } from '../../context/context';
-import client from '../../feathers';
-import AuthWrapper from '../../helper/AuthWrapper';
+import Button from "../../components/buttons/Button";
+import CheckboxInput from "../../components/inputs/basic/Checkbox";
+import Input from "../../components/inputs/basic/Input";
+import PasswordInput from "../../components/inputs/basic/Password";
+import { UserContext } from "../../context/context";
+import client from "../../feathers";
+import AuthWrapper from "../../helper/AuthWrapper";
+
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+
+const schema = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+}).required();
+
 
 function Login() {
   const navigate = useNavigate();
-  const { handleSubmit, control } = useForm();
+  const { control, handleSubmit, formState:{ errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+
   const { setUser } = useContext(UserContext);
+  
+
 
   useEffect(() => {
     window.scrollTo({
       top: 0,
       left: 0,
     });
-    document.title = 'Health Stack - Login';
+    document.title = "Health Stack - Login";
   }, []);
 
   const onSubmit = ({ email, password }) => {
     client
       .authenticate({
-        strategy: 'local',
+        strategy: "local",
         email,
         password,
       })
       .then((res) => {
         setUser(res.user);
-        localStorage.setItem('user', JSON.stringify(res.user));
-        navigate('/app');
+        localStorage.setItem("user", JSON.stringify(res.user));
+        navigate("/app");
       })
       .catch((err) => {
         toast.error(`Error loggin in User, probable network issues ${err}`);
-        navigate('/app');
+        navigate("/app");
       });
   };
 
@@ -49,14 +65,21 @@ function Login() {
           name="email"
           control={control}
           render={({ field }) => (
-            <Input {...field} label="Email" placeholder="Enter your email" />
+            <Input {...field} 
+              label="Email" 
+              placeholder="Enter your email" 
+            />
           )}
         />
+        <p style={{color: "red", fontSize: "16px" }}>{errors.email?.message}</p>
+
         <Controller
           name="password"
           control={control}
           render={({ field }) => <PasswordInput {...field} />}
         />
+        <p style={{color: "red", fontSize: "16px" }}>{errors.password?.message}</p>
+
         <CheckboxInput label="Keep me Logged in" />
         <Button type="submit" label="Login" fullwidth />
       </form>
@@ -78,10 +101,10 @@ function Login() {
           <Link
             className="nav-link"
             style={{
-              padding: '0',
-              background: 'transparent',
-              color: 'blue',
-              marginLeft: '0.6rem',
+              padding: "0",
+              background: "transparent",
+              color: "blue",
+              marginLeft: "0.6rem",
             }}
             to="/signup"
           >
