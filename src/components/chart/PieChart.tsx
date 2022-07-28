974f8a3a04afab3dd14b75b883c7db18a1e44bfc
daregoodness1997 <daregoodness@gmail.com>
2022-07-28 23:25:07 +0100

    pie chart

--- src/components/chart/PieChart.tsx
+++ src/components/chart/PieChart.tsx
@@ -3,15 +3,23 @@ import Chart from 'react-apexcharts';
 
 const PieChart = () => {
   const state = {
-    options: {},
-    series: [44, 55, 41, 17, 15],
+    options: {
+      plotOptions: {
+        pie: {
+          donut: {
+            size: '45%',
+          },
+        },
+      },
+    },
+    data: [44, 55, 41, 17, 15],
     labels: ['A', 'B', 'C', 'D', 'E'],
   };
   return (
     <div>
       <Chart
         options={state.options}
-        series={state.series}
+        series={state.data}
         type='donut'
         width='380'
       />
