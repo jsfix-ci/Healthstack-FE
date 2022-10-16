04df9d4c8b53596cfcb4c312e7092e50054e71d8
daregoodness1997 <daregoodness@gmail.com>
2022-10-16 22:33:36 +0100

    routing issues

--- src/hsmodules/PrivateOutlet.tsx
+++ src/hsmodules/PrivateOutlet.tsx
@@ -4,7 +4,8 @@ import { UserContext } from '../context/context';
 import Dashboard from '../pages/Dashboard';
 
 const PrivateOutlet = () => {
-  const { user } = useContext(UserContext);
+  // const { user } = useContext(UserContext);
+  const user = true;
   return user ? <Dashboard /> : <div>Access Denied</div>;
 };
 
